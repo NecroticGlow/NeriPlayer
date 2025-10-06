a2e2db1242d6ec9a84f02832b7e19d2d8736aef5
NecroticGlow <157957530+NecroticGlow@users.noreply.github.com>
2025-10-06 20:47:44 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,32 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
+    // Android application plugin
     alias(libs.plugins.android.application) apply false
+    // Kotlin plugin for Android
     alias(libs.plugins.kotlin.android) apply false
 }
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Kotlin Gradle plugin version
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
+        // Android Gradle plugin version
+        classpath("com.android.tools.build:gradle:7.0.2")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+task("clean", type = Delete::class) {
+    delete(rootProject.buildDir)
+}
+
